51228331fcfa426c5320ba196976df3ceec4304b
nachoeg <nachoeg99@gmail.com>
2023-05-07 15:40:22 -0300

    error 404 imagen

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,13 @@
 function NotFoundPage() {
-	return <h1 style={{ textAlign: 'center' }}>Error 404 not found</h1>;
+	return (
+		<div style={{ display: 'flex', justifyContent: 'center' }}>
+			<img
+				style={{ width: '80vw', height: '80vh' }}
+				src="/404error.svg"
+				alt="Error 404 not found"
+			/>
+		</div>
+	);
 }
 
 export default NotFoundPage;
