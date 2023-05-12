55aba5b7d0eb766a732c0aa4dd1ef76c40300112
nachoeg <nachoeg99@gmail.com>
2023-05-12 03:05:36 -0300

    ruta añadidas

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import CssBaseline from '@mui/material/CssBaseline';
 import SignUpPage from './pages/SignUpPage';
+import UsersPage from './pages/UsersPage';
 // import { ThemeProvider } from '@mui/material/styles';
 
 import './App.css';
 import Navbar from './components/Navbar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
 	const theme = createTheme({
@@ -37,6 +39,8 @@ function App() {
 						<Route index element={<HomePage />} />
 						<Route path="login" element={<LogInPage />} />
 						<Route path="signup" element={<SignUpPage />} />
+						<Route path="perfil" element={<ProfilePage />} />
+						<Route path="usuarios" element={<UsersPage />} />
 						<Route path="*" element={<NotFoundPage />} />
 					</Route>
 				</Routes>
