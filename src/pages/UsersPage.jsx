40417fe531d1ed9cf647defa1ed1b3c4651541f8
nachoeg <nachoeg99@gmail.com>
2023-05-14 13:20:05 -0300

    fetch usuarios

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -2,8 +2,31 @@ import AddIcon from '@mui/icons-material/Add';
 import { Container } from '@mui/material';
 import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom';
+import url from '../data/url';
 
 function UsersPage() {
+	const token = localStorage.getItem('jwt');
+	fetch(url + 'usuarios', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `${token}`,
+		},
+	})
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw new Error('Error en la solicitud fetch');
+			}
+		})
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((error) => {
+			console.error('Error en el fetch: ' + error);
+		});
+
 	return (
 		<Container
 			component="main"
