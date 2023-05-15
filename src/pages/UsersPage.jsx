6d7dcb9b9b66434c9e90672f696a30446ea35925
nachoeg <nachoeg99@gmail.com>
2023-05-15 01:59:00 -0300

    fetch usuarios, y actualizacion de tabla

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -4,75 +4,89 @@ import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom';
 import { DataGrid } from '@mui/x-data-grid';
 import url from '../data/url';
+import { useEffect, useState } from 'react';
 
 function UsersPage() {
 	const token = localStorage.getItem('jwt');
-	fetch(url + 'usuarios', {
-		method: 'GET',
-		credentials: 'include',
-		headers: {
-			'Content-Type': 'application/json',
-			token: `${token}`,
-		},
-	})
-		.then((response) => {
-			if (response.ok) {
-				return response.json();
-			} else {
-				throw new Error('Error en la solicitud fetch');
-			}
+	const [rows, setRows] = useState([]);
+	useEffect(() => {
+		obtenerUsuarios().then((rows) => setRows(rows));
+	}, []);
+	function obtenerUsuarios() {
+		return fetch(url + 'usuarios', {
+			method: 'GET',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+				token: `${token}`,
+			},
 		})
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((error) => {
-			console.error('Error en el fetch: ' + error);
-		});
-
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error('Error en la solicitud fetch');
+				}
+			})
+			.then((data) => {
+				// console.log(data);
+				return data;
+			})
+			.catch((error) => {
+				console.error('Error en el fetch: ' + error);
+			});
+	}
 	const columns = [
-		{ field: 'id', headerName: 'ID', width: 70 },
-		{ field: 'firstName', headerName: 'First name', width: 130 },
-		{ field: 'lastName', headerName: 'Last name', width: 130 },
+		{ field: 'id', headerName: 'ID', width: 50 },
 		{
-			field: 'age',
-			headerName: 'Age',
+			field: 'email',
+			headerName: 'Email',
+			width: 170,
+			editable: true,
+		},
+		{
+			field: 'password',
+			headerName: 'Contraseña',
+			width: 100,
+		},
+		{ field: 'nombre', headerName: 'Nombre', width: 100 },
+		{ field: 'apellido', headerName: 'Apellido', width: 100 },
+		{
+			field: 'telefono',
+			headerName: 'Telefono',
 			type: 'number',
-			width: 90,
+			width: 100,
 		},
 		{
-			field: 'fullName',
-			headerName: 'Full name',
-			description: 'This column has a value getter and is not sortable.',
-			sortable: false,
-			width: 160,
-			valueGetter: (params) =>
-				`${params.row.firstName || ''} ${params.row.lastName || ''}`,
+			field: 'localidad',
+			headerName: 'Localidad',
+			width: 100,
 		},
-	];
-
-	const rows = [
-		{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-		{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-		{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-		{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-		{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-		{ id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-		{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-		{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-		{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+		{
+			field: 'dni',
+			headerName: 'Dni',
+			type: 'number',
+			width: 100,
+		},
+		{
+			field: 'rol',
+			headerName: 'Rol',
+			width: 100,
+		},
 	];
 
 	return (
 		<Container
 			component="main"
-			maxWidth="md"
+			maxWidth="lg"
 			sx={{
 				display: 'flex',
 				alignItems: 'center',
 				flexDirection: 'column',
+				mt: 8,
 			}}
 		>
-			<div style={{ height: 400, width: '100%', marginTop: '20px' }}>
+			<div style={{ height: 400, width: '100%' }}>
 				<DataGrid
 					rows={rows}
 					columns={columns}
@@ -82,7 +96,6 @@ function UsersPage() {
 						},
 					}}
 					pageSizeOptions={[5, 10]}
-					checkboxSelection
 				/>
 			</div>
 			<NavLink to="/signup" style={{ textDecoration: 'none', width: '100%' }}>
