53001e584ff8fe52fb32ea47a1f5cf753978e16a
nachoeg <nachoeg99@gmail.com>
2023-06-04 22:05:58 -0300

    correccion animacion

--- src/components/Logotipo.jsx
+++ src/components/Logotipo.jsx
@@ -6,55 +6,67 @@ import { NavLink } from 'react-router-dom';
 import '../css/animaciones.css';
 
 function Logotipo() {
+	const handleClick = (event) => {
+		const logo = event.currentTarget;
+		logo.classList.add('shake');
+	};
+
+	const handleAnimationEnd = (event) => {
+		const logo = event.currentTarget;
+		logo.classList.remove('shake');
+	};
+
 	return (
-		<NavLink
-			to="/"
-			className={'shake'}
-			style={{
-				textDecoration: 'none',
-				color: '#0197b2',
-				display: 'flex',
-			}}
-		>
-			<Avatar
-				sx={{
-					// display: { xs: 'none', md: 'block' },
-					mr: { xs: 0, sm: 1 },
-					alignSelf: 'center',
-				}}
-				alt="Oh My Dog! logo"
-				src="/logo.png"
-			/>
-			<Box
-				sx={{
-					display: { xs: 'none', sm: 'flex' },
-					flexDirection: 'column',
-					marginRight: { xs: 0, sm: 3 },
+		<div onAnimationEnd={handleAnimationEnd} onClick={handleClick}>
+			<NavLink
+				to="/"
+				style={{
+					textDecoration: 'none',
+					color: '#0197b2',
+					display: 'flex',
 				}}
 			>
-				<Typography
-					variant="h5"
-					noWrap
+				<Avatar
 					sx={{
-						fontWeight: 700,
-						textAlign: 'center',
+						// display: { xs: 'none', md: 'block' },
+						mr: { xs: 0, sm: 1 },
+						alignSelf: 'center',
+					}}
+					alt="Oh My Dog! logo"
+					src="/logo.png"
+					className="hueso"
+				/>
+				<Box
+					sx={{
+						display: { xs: 'none', sm: 'flex' },
+						flexDirection: 'column',
+						marginRight: { xs: 0, sm: 3 },
 					}}
 				>
-					¡Oh my dog!
-				</Typography>
-				<Typography
-					variant="subtitle2"
-					noWrap
-					sx={{
-						textAlign: 'center',
-						fontWeight: 500,
-						letterSpacing: '.2rem',
-					}}
-				>
-					VETERINARIA
-				</Typography>
-			</Box>
-		</NavLink>
+					<Typography
+						variant="h5"
+						noWrap
+						sx={{
+							fontWeight: 700,
+							textAlign: 'center',
+						}}
+					>
+						¡Oh my dog!
+					</Typography>
+					<Typography
+						variant="subtitle2"
+						noWrap
+						sx={{
+							textAlign: 'center',
+							fontWeight: 500,
+							letterSpacing: '.2rem',
+						}}
+					>
+						VETERINARIA
+					</Typography>
+				</Box>
+			</NavLink>
+		</div>
 	);
 }
 
