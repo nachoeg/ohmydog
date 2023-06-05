e2d9af4b9c5c596b8d7f2eac21b52fe5f128d9fb
nachoeg <nachoeg99@gmail.com>
2023-06-04 22:14:07 -0300

    boton para registrar solo aparece si sos usuario cliente

--- src/pages/AdoptPage.jsx
+++ src/pages/AdoptPage.jsx
@@ -1,9 +1,17 @@
 import { Button, Container } from '@mui/material';
 import TablaAdopcion from '../components/TablaAdopcion';
 import { NavLink } from 'react-router-dom';
+import { Context } from '../context/Context';
 import { Add } from '@mui/icons-material';
+import { useContext, useEffect, useState } from 'react';
 
 function AdoptPage() {
+	const { usuario } = useContext(Context);
+	const [cliente, setCliente] = useState('');
+	useEffect(() => {
+		if (usuario) setCliente(usuario.rol == 'cliente');
+	});
+
 	return (
 		<Container
 			component="main"
@@ -16,18 +24,20 @@ function AdoptPage() {
 			}}
 		>
 			<TablaAdopcion />
-			<NavLink
-				to="/adopcion/registrar"
-				style={{ textDecoration: 'none', width: '100%' }}
-			>
-				<Button
-					startIcon={<Add />}
-					sx={{ mt: 2, width: '100%' }}
-					variant="contained"
+			{cliente && (
+				<NavLink
+					to="/adopcion/registrar"
+					style={{ textDecoration: 'none', width: '100%' }}
 				>
-					Registrar perro para adopción
-				</Button>
-			</NavLink>
+					<Button
+						startIcon={<Add />}
+						sx={{ mt: 2, width: '100%' }}
+						variant="contained"
+					>
+						Registrar perro para adopción
+					</Button>
+				</NavLink>
+			)}
 		</Container>
 	);
 }
