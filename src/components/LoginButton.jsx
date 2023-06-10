73369be40a135eda63a0e7d4e079a261707ecaa1
nachoeg <nachoeg99@gmail.com>
2023-06-09 22:30:34 -0300

    login: el boton para ir '/login' desaparece

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,29 +1,41 @@
 import Button from '@mui/material/Button';
 import LoginIcon from '@mui/icons-material/Login';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
+import { useEffect, useState } from 'react';
 
 function LoginButton() {
+	const location = useLocation();
+	const [mostrar, setMostrar] = useState();
+	useEffect(() => {
+		setMostrar(location.pathname != '/login');
+	}, [location]);
 	return (
-		<NavLink to="/login">
-			<Button
-				variant="contained"
-				sx={{
-					paddingBlock: { xs: '12px', md: '6px' },
-					paddingInline: { xs: '12px', md: '16px' },
-					minWidth: { xs: '36px' },
-				}}
-				startIcon={<LoginIcon sx={{ marginLeft: { xs: '4px', md: '0px' } }} />}
-			>
-				<Typography
-					sx={{
-						display: { xs: 'none', md: 'flex' },
-					}}
-				>
-					Iniciar sesión
-				</Typography>
-			</Button>
-		</NavLink>
+		<>
+			{mostrar && (
+				<NavLink to="/login">
+					<Button
+						variant="contained"
+						sx={{
+							paddingBlock: { xs: '12px', md: '6px' },
+							paddingInline: { xs: '12px', md: '16px' },
+							minWidth: { xs: '36px' },
+						}}
+						startIcon={
+							<LoginIcon sx={{ marginLeft: { xs: '4px', md: '0px' } }} />
+						}
+					>
+						<Typography
+							sx={{
+								display: { xs: 'none', md: 'flex' },
+							}}
+						>
+							Iniciar sesión
+						</Typography>
+					</Button>
+				</NavLink>
+			)}
+		</>
 	);
 }
 
